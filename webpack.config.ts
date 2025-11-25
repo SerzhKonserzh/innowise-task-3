6cedeb3651d1edc23505691f2101e138dd0bb444
Sergey <Serzh-2001BTR@mail.ru>
2025-11-25 17:29:03 +0300

    feat: add basic webpack configuration for React + TS

---
+++ webpack.config.ts
@@ -0,0 +1,63 @@
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+
+type Mode = "production" | "development"; 
+
+//interface для переменных окружения
+interface EnvVariables {
+  mode: Mode;
+  port: number;
+}
+
+export default (env: EnvVariables) => {
+
+  const isDev = env.mode === 'development';
+
+  const config: webpack.Configuration = {
+    //режим разработки
+    mode: env.mode ?? "development", 
+    //входной файл
+    entry: path.resolve(__dirname, "src", "index.tsx"), 
+    //выходной файл
+    output: {
+      filename: "[name].[contenthash].js", //каждый раз подставляет хэш от контента
+      path: path.resolve(__dirname, "build"), 
+      clean: true,
+    }, 
+    //подключаемые плагины
+    plugins: [ 
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html"),
+      }),
+    ],
+    module: {
+      //массив loader в необходимом порядке
+      rules: [
+        //настройка отдельного loader
+        {
+          //регулярное выражение для проверки файлов на .ts и .tsx
+          test: /\.tsx?$/, 
+          //используемый лоадер
+          use: "ts-loader", 
+          //то, что не обрабатываем
+          exclude: /node_modules/, 
+        },
+      ],
+    },
+    resolve: {
+      // расширения, которые необходимо обрабатывать
+      extensions: [".tsx", ".ts", ".js"], 
+    },
+    //создает карту исходного кода, упрощает поиск ошибок
+    devtool: isDev ? 'inline-source-map' : false, 
+    //настройка devServer
+    devServer: isDev ? { 
+      port: env.port ?? 3000,
+      open: true,
+    } : undefined,
+  };
+
+  return config;
+};
